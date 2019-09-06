63fb74cb836ef13b778907817f6e292cde759835
root <root@ip-172-26-4-211.ap-southeast-2.compute.internal>
2019-09-06 17:00:39 +1000

    update

--- glances_install.sh
+++ glances_install.sh
@@ -5,6 +5,8 @@
 #
 # Copyright (C) 2019 Peter Jiping Xie <peter.jp.xie@gmail.com>
 
+# set -x
+
 SYS_DT="$(date +%F-%T)"
 
 exiterr()  { echo "Error: $1" >&2; exit 1; }
@@ -27,7 +29,7 @@ if ! printf '%s' "$os_type" | head -n 1 | grep -qiF -e ubuntu -e debian; then
 fi
 
 # Require Ubuntu 16.04 or above
-if [ "$os_type" = "Ubuntu"]; then
+if [ "$os_type" = "Ubuntu" ]; then
     # ubuntu_version = "$(lsb_release -r 2>/dev/null)"
     lsb_release -r 2>/dev/null | awk '
     { if($2 < 16.04)
@@ -36,7 +38,7 @@ if [ "$os_type" = "Ubuntu"]; then
 fi
 
 # Use with caution with debian, not tested though it may work
-if [ "$os_type" != "Ubuntu"]; then
+if [ "$os_type" != "Ubuntu" ]; then
     echo "Use with CAUTION! debian is not tested though it should work."
 fi
 
@@ -62,36 +64,39 @@ echo "### Installing glances... ###"
 pip3 install -U glances bottle influxdb
 
 # copy default glances settings. Default is good enough.
+mkdir -p /etc/glances
 cp /usr/local/share/doc/glances/glances.conf /etc/glances/glances.conf
 
 
 ### Install  influxdb ###
 # ref: https://docs.influxdata.com/influxdb/v1.7/introduction/installation/
-echo "### Installing glances... ###"
+echo "### Installing influxdb... ###"
 
 # add InfluxData repository 
-if [ "$os_type" = "Ubuntu"]; then 
+if [ "$os_type" = "Ubuntu" ]; then 
   # ubuntu
   wget -qO- https://repos.influxdata.com/influxdb.key | apt-key add -
-  source /etc/lsb-release
-  echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | tee /etc/apt/sources.list.d/influxdb.list
+  . /etc/lsb-release && echo "deb https://repos.influxdata.com/ubuntu $DISTRIB_CODENAME stable" | tee /etc/apt/sources.list.d/influxdb.list
 else 
   # debian
   wget -qO- https://repos.influxdata.com/influxdb.key | apt-key add -
-  source /etc/os-release
-  test $VERSION_ID = "7" && echo "deb https://repos.influxdata.com/debian wheezy stable" | tee /etc/apt/sources.list.d/influxdb.list
-  test $VERSION_ID = "8" && echo "deb https://repos.influxdata.com/debian jessie stable" | tee /etc/apt/sources.list.d/influxdb.list
-  test $VERSION_ID = "9" && echo "deb https://repos.influxdata.com/debian stretch stable" | tee /etc/apt/sources.list.d/influxdb.list
+  . /etc/os-release
+  test "$VERSION_ID" = "7" && echo "deb https://repos.influxdata.com/debian wheezy stable" | tee /etc/apt/sources.list.d/influxdb.list
+  test "$VERSION_ID" = "8" && echo "deb https://repos.influxdata.com/debian jessie stable" | tee /etc/apt/sources.list.d/influxdb.list
+  test "$VERSION_ID" = "9" && echo "deb https://repos.influxdata.com/debian stretch stable" | tee /etc/apt/sources.list.d/influxdb.list
 fi
 
 # install 
-apt-get update && apt-get install influxdb
-# service influxdb start
-systemctl start influxdb
+apt-get update && apt-get -yq install influxdb
+# It starts service automatically after installation.
+# systemctl start influxdb
+
 # enable service start on bootup
 systemctl enable influxdb
 
 # create database "glances" via REST API. This is db name in glances default config.
+echo "Wait 5 seconds for influxdb to start."
+sleep 5
 curl -XPOST 'http://localhost:8086/query' --data-urlencode 'q=CREATE DATABASE "glances"'   
 curl -XPOST 'http://localhost:8086/query' --data-urlencode 'q=show databases'  
 
@@ -99,11 +104,13 @@ curl -XPOST 'http://localhost:8086/query' --data-urlencode 'q=show databases'
 # ref: https://grafana.com/docs/installation/debian/
 echo "### Installing Grafana ... ###"
 
-apt-get install -y software-properties-common
-add-apt-repository "deb https://packages.grafana.com/oss/deb stable main"
+# This may fail, remove it since it seems not neccesary.
+# apt-get install -y software-properties-common
+# add-apt-repository "deb https://packages.grafana.com/oss/deb stable main" 
+
 wget -q -O - https://packages.grafana.com/gpg.key | apt-key add -
 apt-get update
-apt-get install grafana
+apt-get -yq install grafana
 systemctl start grafana-server
 # enable service start on bootup
 systemctl enable grafana-server
@@ -113,7 +120,19 @@ systemctl enable grafana-server
 # influxdb: TCP 8086, 8088
 # grafana: TCP 3000
 
+# Run the rules
+# Added for glances web - optional
+iptables -I INPUT -p tcp --dport 61208 -j ACCEPT
+
+# Added for Influxdb
+iptables -I INPUT -p tcp --dport 8086 -j ACCEPT
+iptables -I INPUT -p tcp --dport 8088 -j ACCEPT
+
+# Added for Grafana
+iptables -I INPUT -p tcp --dport 3000 -j ACCEPT
+
 # Add iptable rules to /etc/rc.local so it will take effect on bootup.
+# You can also modify /etc/iptables.rules.
 
 # create rc.local if not exist.
 if [ -f /etc/rc.local ]; then
@@ -139,10 +158,15 @@ iptables -I INPUT -p tcp --dport 3000 -j ACCEPT
 exit 0
 EOF
 
-echo "Congrats!
+
+cat <<EOF
+
+================================================
+
+Congrats!
 You have installed Glances, Influxdb and Grafana. And all services are running.
 
-To do:
+### To do ###
 If you want to access Grafana portal remotely, you need to open below ports in your firewall settings other than iptables rules if there is any.
 (iptables rules have been taken cared of.)
 
@@ -150,9 +174,12 @@ glances web - optional: TCP 61208
 influxdb: TCP 8086, 8088
 grafana: TCP 3000
 
+### To use ###
 Run this command to start collecting data:
 glances --export influxdb
 
 Then create Grafana dashboards to view data:
 http://<server ip>:3000/
-" 
+
+================================================
+EOF
